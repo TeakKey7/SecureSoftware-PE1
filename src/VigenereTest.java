9200eb7b949d45779aef1e0d900a4f2bb0c7f16a
Caleb Metz <caleb@teakkey.com>
2026-02-28 17:14:58 -0600

    Basic vigenere test done

--- src/VigenereTest.java
+++ src/VigenereTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * CEN 4078 Programming Exercise 1
  * File Name: null.java
@@ -9,10 +11,38 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  */
 public class VigenereTest {
+
+    Vigenere vigenere = new Vigenere();
+
+    String alphaKey = "ARGOSROCK";
+
     @Test
-    void testAlphaEncrypt() {
-        
+    void testBasicAlphaEncrypt() {
+        String basicAlpha = "ILikeCheesecake";
+        String basicAlphaResult = "ICoywTvgosviocv";
+
+        StringBuilder dummyStringBuilder = new StringBuilder();
+
+        for (int i = 0; i < alphaKey.length(); i++) {
+            dummyStringBuilder.append("A");
+        }
+
+        String dummyString = dummyStringBuilder.toString();
+
+        assertEquals(alphaKey, vigenere.encrypt(alphaKey, dummyString), "String of 'A's should return the key");
+        assertEquals(basicAlphaResult,vigenere.encrypt(alphaKey, basicAlpha), "Encrypt basic string");
+
+
     }
+
+    @Test
+    void testFullAlphaEncrypt() {
+        String fullPassword = "Password12345";
+        String fullPasswordResult = "Prygofff23570";
+
+        assertEquals(fullPasswordResult,vigenere.encrypt(alphaKey, fullPassword), "Encrypt basic string");
+    }
+
     @Test
     void testAlphaDecrypt() {
 
@@ -22,5 +52,7 @@ public class VigenereTest {
 
     }
     @Test
-    void testNumberDecrypt
+    void testNumberDecrypt() {
+
+    }
 }
